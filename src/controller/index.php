5227443255f4df739f333f97b2fd54af652033a3
Eugene <eugenevaleska1994@gmail.com>
2018-04-15 02:37:32 +0300

    Demo page is working.

---
+++ src/controller/index.php
@@ -0,0 +1,9 @@
+<?php
+    require_once "../../vendor/autoload.php";
+
+    $loader = new Twig_Loader_Filesystem("../../tpl");
+    $twig = new Twig_Environment($loader);
+
+    echo $twig->render("index.tpl.twig", [
+
+    ]);
