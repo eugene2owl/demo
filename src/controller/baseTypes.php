fd135397ec9afd64491b62ee3f8c33ccd7de3f2b
Eugene <eugenevaleska1994@gmail.com>
2018-04-17 20:39:14 +0300

    Code coloring added. Code processor class created.

--- src/controller/baseTypes.php
+++ src/controller/baseTypes.php
@@ -4,12 +4,17 @@ namespace Demo\Controller;
 
 require_once "../../vendor/autoload.php";
 require_once "../service/Contents.php";
+require_once "../service/CodeProcessor.php";
 
+use Demo\Service\CodeProcessor;
 use Demo\Service\Contents as ContentsService;
 
 $tunnelToDB = new ContentsService();
 $contents = $tunnelToDB->getContents(basename(__FILE__));
+
 $codes = $tunnelToDB->getEntityArray("codes");
+$codeProcessor = new CodeProcessor();
+$codes = $codeProcessor->processCodes($codes);
 
 $loader = new \Twig_Loader_Filesystem("../../tpl");
 $twig = new \Twig_Environment($loader);
@@ -17,6 +22,8 @@ $twig = new \Twig_Environment($loader);
 echo $twig->render("baseTypes.tpl.twig", [
     "title"        => $contents["titles"][0]["title"],
     "header"       => $contents["titles"][0]["title"],
-    "article_1"    => $contents["articles"][0]["article"],
+    "article_1"    => "<h1>" . $contents["articles"][0]["article"] . "</h1>",
     "codes"        => $codes,
 ]);
+
+echo "<span style='text-decoration: underline'>161</span>";
---
+++ src/service/CodeProcessor.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Demo\Service;
+
+class CodeProcessor
+{
+    private const STRING_COLOR = "#FF3537";
+    private const FUNCTION_COLOR = "#2B50DE";
+    private const VARIABLE_COLOR = "#E7A803";
+
+    private const STRING_REGEX = "/((\".+\")|('.+'))/";
+    private const FUNCTION_REGEX = "/[a-zA-Z0-9_]+\(/";
+    private const VARIABLE_REGEX = "/\$[a-zA-Z0-9_]+/";
+
+    private function getEntranceArray(string $text, string $patternRegex): array
+    {
+        $entranceArray = [];
+        preg_match_all(
+            $patternRegex,
+            $text,
+            $matchArray,
+            PREG_OFFSET_CAPTURE
+        );
+        foreach ($matchArray[0] as $key => $secondLevelArray) {
+            if ($patternRegex !== self::FUNCTION_REGEX) {
+                $entranceArray[$key]["length"] = strlen($secondLevelArray[0]);
+            } else {
+                $entranceArray[$key]["length"] = strlen($secondLevelArray[0]) - 1;
+            }
+            $entranceArray[$key]["position"] = $secondLevelArray[1];
+        }
+        return $entranceArray;
+    }
+
+    private function insertPatternInString(string $string, string $pattern, int $position): string
+    {
+        $string = substr_replace($string, $pattern, $position, 0);
+        return $string;
+    }
+
+    private function wrapUpInSpan(string $text, string $regexPattern, string $style): string
+    {
+        $positions = $this->getEntranceArray($text, $regexPattern);
+        for ($entranceNumber = count($positions) - 1; $entranceNumber > -1; $entranceNumber--) {
+            $text = $this->insertPatternInString(
+                $text,
+                "</span>",
+                $positions[$entranceNumber]["position"] + $positions[$entranceNumber]["length"]
+            );
+            $text = $this->insertPatternInString(
+                $text,
+                "<span style='$style'>",
+                $positions[$entranceNumber]["position"]
+            );
+        }
+        return $text;
+    }
+
+    private function processCode(string $code): string
+    {
+        $code = $this->wrapUpInSpan(
+            $code,
+            self::STRING_REGEX,
+            "color: " . self::STRING_COLOR
+        );
+        $code = $this->wrapUpInSpan(
+            $code,
+            self::VARIABLE_REGEX,
+            "color: " . self::VARIABLE_REGEX
+        );
+        $code = $this->wrapUpInSpan(
+            $code,
+            self::FUNCTION_REGEX,
+            "color: " . self::FUNCTION_COLOR
+        );
+        return $code;
+    }
+
+    private function processOutput(string $output):string
+    {
+        $output = $this->wrapUpInSpan(
+            $output,
+            self::STRING_REGEX,
+            "color: #FF3537"
+        );
+        return $output;
+    }
+
+    public function processCodes(array $codes): array
+    {
+        foreach ($codes as $number => $code) {
+            $codes[$number]["code"] = $this->processCode($code["code"]);
+            $codes[$number]["output"] = $this->processOutput($code["output"]);
+        }
+        return $codes;
+    }
+}
