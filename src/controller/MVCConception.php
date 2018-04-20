ddf697c56c263d627d3b4e095f73bc89b2d3b3f7
Eugene <eugenevaleska1994@gmail.com>
2018-04-21 02:15:06 +0300

    MVC conception page goes under new database.

--- src/controller/MVCConception.php
+++ src/controller/MVCConception.php
@@ -4,37 +4,45 @@ declare(strict_types = 1);
 
 namespace Demo\Controller;
 
+require_once "const.php";
 require_once "../../vendor/autoload.php";
 require_once "../service/Contents.php";
 require_once "../service/LinkInserter.php";
 require_once "../service/StyleChanger.php";
+require_once "../service/ListProcessor.php";
 
 use Demo\Service\Contents as ContentsService;
 use Demo\Service\LinkInserter;
 use Demo\Service\StyleChanger;
+use Demo\Service\ListProcessor;
 
 $tunnelToDB = new ContentsService();
-$contents = $tunnelToDB->getContents(basename(__FILE__));
+$pageContents = $tunnelToDB->getContentsFromPage(basename(__FILE__));
+
+$listProcessor = new ListProcessor();
+$lists = $listProcessor->clarifyLists($pageContents["lists"]);
 
 $linkInserter = new LinkInserter();
 $links = [
 
 ];
-$contents["articles"] = $linkInserter->insertLinksIntoTextes($links, $contents["articles"]);
+$pageContents["articles"] = $linkInserter->insertLinksIntoTexts($links, $pageContents["articles"]);
 
 $styleChanger = new StyleChanger();
 $radiobuttonValues = $styleChanger->getRadiobuttonValues();
 $styleMode = $styleChanger->getStyleMode($_POST["styleMode"]);
 
-$loader = new \Twig_Loader_Filesystem("../../tpl/");
+$loader = new \Twig_Loader_Filesystem(TEMPLATES_PATH_FOR_TWIG);
 $twig = new \Twig_Environment($loader);
 
 echo $twig->render("MVCConception.tpl.twig", [
     "style"                => $styleMode,
-    "title"                => $contents["titles"][0]["title"],
-    "header"               => $contents["titles"][0]["title"],
-    "articles"             => $contents["articles"],
-    "title_1"              => "So what are we getting?",
-    "lists"                => $contents["lists"],
+    "title"                => $pageContents["titles"][0]["name"],
+    "header"               => $pageContents["titles"][0]["name"],
+    "articles"             => $pageContents["articles"],
+    "title_1"              => $pageContents["titles"][1]["name"],
+    "lists"                => $lists,
     "radiobuttonValues"    => $radiobuttonValues,
+    "image_1_src"          => IMAGES_FOLDER_PATH . $pageContents["images"][0]["name"],
+    "image_2_src"          => IMAGES_FOLDER_PATH . $pageContents["images"][1]["name"],
 ]);
