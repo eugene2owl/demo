7db75e0b5458f1bf0cbfcab9d7e7cfd20ef94525
Eugene <eugenevaleska1994@gmail.com>
2018-04-21 03:14:42 +0300

    Writing style page goes under new database.

--- src/controller/writingStyle.php
+++ src/controller/writingStyle.php
@@ -4,27 +4,33 @@ declare(strict_types = 1);
 
 namespace Demo\Controller;
 
+require_once "const.php";
 require_once "../../vendor/autoload.php";
 require_once "../service/Contents.php";
 require_once "../service/CodeProcessor.php";
+require_once "../service/ListProcessor.php";
 
 use Demo\Service\CodeProcessor;
 use Demo\Service\Contents as ContentsService;
+use Demo\Service\ListProcessor;
 
 $tunnelToDB = new ContentsService();
-$contents = $tunnelToDB->getContents(basename(__FILE__));
+$pageContents = $tunnelToDB->getContentsFromPage(basename(__FILE__));
 
-$codes = $contents["codes"];
+$codes = $tunnelToDB->getCodesWithAttachmentsFromPage(basename(__FILE__));
 $codeProcessor = new CodeProcessor();
 $codes = $codeProcessor->processCodes($codes);
 
-$loader = new \Twig_Loader_Filesystem("../../tpl/");
+$listProcessor = new ListProcessor();
+$codes = $listProcessor->clarifyCodesLists($codes);
+
+$loader = new \Twig_Loader_Filesystem(TEMPLATES_PATH_FOR_TWIG);
 $twig = new \Twig_Environment($loader);
 
 echo $twig->render("writingStyle.tpl.twig", [
-    "title"       => $contents["titles"][0]["title"],
-    "header"      => $contents["titles"][0]["title"],
-    "articles"    => $contents["articles"],
+    "title"       => $pageContents["titles"][0]["name"],
+    "header"      => $pageContents["titles"][0]["name"],
+    "articles"    => $pageContents["articles"],
     "codes"       => $codes,
-    "lists"       => $contents["lists"],
+    "lists"       => $pageContents["lists"],
 ]);
