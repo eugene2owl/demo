ae5916e1ff84aa52ac05b73091a9f923d7095255
Eugene <eugenevaleska1994@gmail.com>
2018-04-21 14:05:55 +0300

    One page left.

--- src/controller/DBInteraction.php
+++ src/controller/DBInteraction.php
@@ -4,43 +4,45 @@ declare(strict_types = 1);
 
 namespace Demo\Controller;
 
+require_once "const.php";
 require_once "../../vendor/autoload.php";
 require_once "../service/Contents.php";
 require_once "../service/CodeProcessor.php";
 require_once "../service/LinkInserter.php";
+require_once "../service/ListProcessor.php";
 
 use Demo\Service\Contents as ContentsService;
 use Demo\Service\CodeProcessor;
 use Demo\Service\LinkInserter;
+use Demo\Service\ListProcessor;
 
 $tunnelToDB = new ContentsService();
-$contents = $tunnelToDB->getContents(basename(__FILE__));
-$titles = $tunnelToDB->getEntityArray("titles");
+$pageContents = $tunnelToDB->getContentsFromPage(basename(__FILE__));
+$pageTitleCouples = $tunnelToDB->getPageTitleCouples();
 
-$codes = $contents["codes"];
+$codes = $tunnelToDB->getCodesWithAttachmentsFromPage(basename(__FILE__));;
 $codeProcessor = new CodeProcessor();
 $codes = $codeProcessor->processCodes($codes);
 
-$linkInserter = new LinkInserter();
-$links = [
-    "long way"       => $titles[5]["page"],
-    "PDO"            => $contents["links"][0]["link"],
-    "mysql\(\)"      => $contents["links"][1]["link"],
-    "prepare\(\)"    => $contents["links"][2]["link"],
-    "execute\(\)"    => $contents["links"][3]["link"],
-    "query\(\)"      => $contents["links"][4]["link"],
-    "exec\(\)"       => $contents["links"][5]["link"],
-];
-$contents["articles"] = $linkInserter->insertLinksIntoTextes($links, $contents["articles"]);
+$listProcessor = new ListProcessor();
+$lists = $listProcessor->clarifyLists($pageContents["lists"]);
 
-$loader = new \Twig_Loader_Filesystem("../../tpl");
+$linkInserter = new LinkInserter();
+$externalLinks = $tunnelToDB->getLinksAssociationsFromPage(basename(__FILE__));
+$innerLinks = [
+    "long way"       => $pageTitleCouples[5]["page"],
+];
+$links = array_merge($externalLinks, $innerLinks);
+$pageContents["articles"] = $linkInserter->insertLinksIntoTexts($links, $pageContents["articles"]);
+
+$loader = new \Twig_Loader_Filesystem(TEMPLATES_PATH_FOR_TWIG);
 $twig = new \Twig_Environment($loader);
 
 echo $twig->render("DBInteraction.tpl.twig", [
-    "title"        => $contents["titles"][0]["title"],
-    "header"       => $contents["titles"][0]["title"],
-    "articles"     => $contents["articles"],
-    "header_1"     => "Details",
+    "title"        => $pageContents["titles"][0]["name"],
+    "header"       => $pageContents["titles"][0]["name"],
+    "articles"     => $pageContents["articles"],
+    "header_1"     => $pageContents["titles"][1]["name"],
     "codes"        => $codes,
-    "lists"        => $contents["lists"],
+    "lists"        => $lists,
 ]);
