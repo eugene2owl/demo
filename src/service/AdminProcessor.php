8305785e11d302b4f0056083b84dabe639ee82d9
Eugene <eugenevaleska1994@gmail.com>
2018-04-24 15:23:13 +0300

    Admin code adding created.

--- src/service/AdminProcessor.php
+++ src/service/AdminProcessor.php
@@ -54,13 +54,137 @@ class AdminProcessor
         ];
     }
 
+    private function validateAddingCode(?string $code): ?string
+    {
+        if (!isset($code)) {
+            return null;
+        }
+        if (strlen($code) > 1) {
+            return $code;
+        }
+        return null;
+    }
+
+    private function getAvailableMainModes(): array
+    {
+        return [
+            1 => "add",
+            2 => "edit",
+            3 => "remove",
+        ];
+    }
+
+    private function getAvailableAddingModes(): array
+    {
+        return [
+            1 => "add code",
+            2 => "add attachment",
+        ];
+    }
+
+    private function getAvailableArticleAttachingModes(): array
+    {
+        return [
+            1 => "find code",
+            2 => "attach to code",
+        ];
+    }
+
+    private function getCurrentMainMode(?string $inputedMainMode): int
+    {
+        if (in_array(
+            (int)$inputedMainMode,
+            array_keys($this->getAvailableMainModes())
+        )) {
+            return (int)$inputedMainMode;
+        }
+        return 1;
+    }
+
+    private function getCurrentAddingMode(?string $inputedAddingMode): int
+    {
+        if (in_array(
+            (int)$inputedAddingMode,
+            array_keys($this->getAvailableAddingModes())
+        )) {
+            return (int)$inputedAddingMode;
+        }
+        return 1;
+    }
+
+    private function getCurrentAddingCode(?string $inputedcode): string
+    {
+        if (
+            is_string($inputedcode) &&
+            strlen($inputedcode) > 2
+        ) {
+            return htmlentities($inputedcode);
+        }
+        return "";
+    }
+
+    private function wasActionCanceled(?string $cancel): bool
+    {
+        return isset($cancel);
+    }
+
+    private function getCurrentAttachedArticle(?string $inputedArticle): ?string
+    {
+        if (
+            is_string($inputedArticle) &&
+            strlen($inputedArticle) > 2
+        ) {
+            return htmlentities($inputedArticle);
+        }
+        return null;
+    }
+
+    private function wasArticleAttachmentSubmitted(?string $attaching): bool
+    {
+        return isset($attaching);
+    }
+
+    private function getCurrentSearchingCode(?string $inputedPattern): ?string
+    {
+        if (
+            is_string($inputedPattern) &&
+            strlen($inputedPattern) > 2
+        ) {
+            return htmlentities("needed code.");
+        }
+        return null;
+    }
+
     private function getAdminPage(): array
     {
+        $availableMainModes = $this->getAvailableMainModes();
+        $availableAddModes = $this->getAvailableAddingModes();
+
+        $mainMode = $this->getCurrentMainMode($_POST["main_option"]);
+        $addingMode = $this->getCurrentAddingMode($_POST["add_option"]);
+
+        $currentAddingCode = $this->getCurrentAddingCode($_POST["code"]);
+        $wasCodeCreated = !empty($currentAddingCode) && !$this->wasActionCanceled($_POST["code_create_cancel"]);
+
+        $currentArticle = $this->getCurrentAttachedArticle($_POST["attaching_article"]) ?? "article is not valid.";
+        $currentSearchingCode = $this->getCurrentSearchingCode($_POST["code_pattern"]) ?? "no code found.";
+        $wasArticleAttached = (
+            $currentArticle !== "article is not valid." &&
+            $currentSearchingCode !== "no code found." &&
+            $this->wasArticleAttachmentSubmitted($_POST["article_attachment_submit"])
+        );
         return ["workshop.tpl.twig", [
-            "mode"                 => $_POST["main_option"],
-            "available_modes"      => ["add", "edit", "remove"],
-            "available_add_modes"  => ["add code", "add attachment"],
-            "add_mode"             => $_POST["add_option"] ?? 1
+            "available_modes"                => $availableMainModes,
+            "available_adding_modes"         => $availableAddModes,
+
+            "adding_mode"                    => $addingMode,
+            "main_mode"                      => $mainMode,
+
+            "was_code_created"               => $wasCodeCreated,
+
+            "attaching_article"              => $currentArticle,
+            "was_article_attached"           => $wasArticleAttached,
+            "searching_code"                 => $currentSearchingCode,
         ]];
     }
 
